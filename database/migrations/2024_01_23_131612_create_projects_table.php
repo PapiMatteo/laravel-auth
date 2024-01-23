81e5a514e489963d640f416b39d3db3abfe8869b
PapiMatteo <matteopapi.work@gmail.com>
2024-01-23 14:43:14 +0100

    add: seeder and route fix

--- database/migrations/2024_01_23_131612_create_projects_table.php
+++ database/migrations/2024_01_23_131612_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,9 +22,10 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])
     ->name('admin.')
-    ->prefix('admi')
+    ->prefix('admin')
     ->group(function(){
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('projects', ProjectController::class)->parameters([]);
     });
 
 require __DIR__.'/auth.php';
